d0c826f6b43b27fb3dd5131897ebd72ac4993014
Diversable <dan.mantei@outlook.com>
2023-12-18 22:25:13 -0800

    update example page & include weak refs support

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
 use leptos::*;
 
 #[component]
-fn ProgressBar(
-    // #[prop(optional)]
-    // #[prop(default = 0)]
-    // #[prop(into)]
-    progress: ReadSignal<i32>,
-    #[prop(optional)]
-    #[prop(default = 100)]
-    max: i32,
-) -> impl IntoView {
-    view! {
-        <p>Progress: {progress} & Max: {max}</p>
-        <progress value=progress max=max></progress>
-    }
-}
-
-#[component]
-fn ListExample(count: ReadSignal<i32>) -> impl IntoView {
-    let vals = vec![1, 2, 3];
-
-    view! {
-        <ul>
-        {vals.into_iter()
-            .map(|n| view!{
-                <li>
-                    "x"{n} " -> " {move || n * count()}
-                </li>
-            })
-            .collect::<Vec<_>>()
-        }
-    </ul>
-    }
-}
-
-#[component]
 fn App(increment: i32) -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
     <div class="container">
+
+            <picture>
+                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
+                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
+            </picture>
+
         <h1>"Welcome to Leptos"</h1>
         <h2><i>"On Github Pages"</i></h2>
 
@@ -52,7 +24,7 @@ fn App(increment: i32) -> impl IntoView {
             {count}
         </button>
 
-        <ListExample count=count />
+
     </div>
     }
 }
